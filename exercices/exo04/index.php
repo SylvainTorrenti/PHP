86e961cf49a3f7c3546349f3a5061fa1492fa562
Sylvain Torrenti <sylvain.torrenti@gmail.com>
2023-04-17 14:57:03 +0200

    Exo 4

--- exercices/exo04/index.php
+++ exercices/exo04/index.php
@@ -1,19 +1,28 @@
 <?php ob_start(); //  ne pas modifier
-$titre = "Exo 04 - Variables"; // Mettre le titre de la page
+$titre = "Exo 04"; // Mettre le titre de la page
 ?>
 
 <!-- mettre votre code ici -->
 <?php
-
+$rand = rand(1, 20);
+if ($rand <= 5) {
+    echo "Il est compris entre 1 et 5";
+} elseif ($rand <= 10) {
+    echo "Il est compris entre 6 et 10";
+} elseif ($rand <= 15) {
+    echo "Il est compris entre 11 et 15";
+} elseif ($rand <= 20) {
+    echo "Il est compris entre 16 et 20";
+}
 
 
 ?>
 
 <?php
-    /**
-     * Ne pa modifier
-     * permet d'inclure le menu et le template
-     */
-    $content = ob_get_clean(); 
-    require "../../partials/layout.php";
+/**
+ * Ne pa modifier
+ * permet d'inclure le menu et le template
+ */
+$content = ob_get_clean();
+require "../../partials/layout.php";
 ?>
