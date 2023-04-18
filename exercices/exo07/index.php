98d061d5a7d81be4db9cc7388a69cc86a7f81bad
Sylvain Torrenti <sylvain.torrenti@gmail.com>
2023-04-18 10:54:04 +0200

    exo 7

--- exercices/exo07/index.php
+++ exercices/exo07/index.php
@@ -4,14 +4,18 @@ $titre = "Exo 07 - Boucles"; // Mettre le titre de la page
 
 <!-- mettre votre code ici -->
 <?php
-
+$rand = rand(5, 15);
+for ($i = 1; $i <= 10; $i++) {
+    $res = $rand * $i;
+    echo "$rand * $i = $res <br>";
+}
 ?>
 
 <?php
-    /**
-     * Ne pa modifier
-     * permet d'inclure le menu et le template
-     */
-    $content = ob_get_clean(); 
-    require "../../partials/layout.php";
+/**
+ * Ne pa modifier
+ * permet d'inclure le menu et le template
+ */
+$content = ob_get_clean();
+require "../../partials/layout.php";
 ?>
