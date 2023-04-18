6d66448c352b2e0ab54a9e6e1865cb49d7f512a0
Sylvain Torrenti <sylvain.torrenti@gmail.com>
2023-04-18 13:36:54 +0200

    exo 8

--- exercices/exo04/index.php
+++ exercices/exo04/index.php
@@ -5,7 +5,11 @@ $titre = "Exo 04"; // Mettre le titre de la page
 <!-- mettre votre code ici -->
 <?php
 $rand = rand(1, 20);
-echo "<h1>Le nombre est : " . $rand . "<br></h1>";
+?>
+<h1>Le nombre est :
+    <?= $rand ?>
+</h1>
+<?php
 if ($rand <= 5) {
     echo "Il est compris entre 1 et 5";
 } elseif ($rand <= 10) {
@@ -14,10 +18,7 @@ if ($rand <= 5) {
     echo "Il est compris entre 11 et 15";
 } elseif ($rand <= 20) {
     echo "Il est compris entre 16 et 20";
-}
-
-
-?>
+} ?>
 
 <?php
 /**
--- exercices/exo08/index.php
+++ exercices/exo08/index.php
@@ -1,18 +1,30 @@
 <?php ob_start(); //  ne pas modifier
-$titre = "Exo 0 - Boucles"; // Mettre le titre de la page
+$titre = "Exo 8"; // Mettre le titre de la page
 ?>
 
 <!-- mettre votre code ici -->
 <?php
+$rand = rand(5, 15);
+$j = 1;
+$result = 0;
+?>
 
-
+<strong>Voici le cumul des
+    <?= $rand ?> premier nombres (sens inverse)<br>
+</strong>
+<?php
+for ($i = $rand; $i >= 1; $i--) {
+    $result += $i;
+    echo "Etape $j - résultat =  $result <br>";
+    $j++;
+}
 ?>
 
 <?php
-    /**
-     * Ne pa modifier
-     * permet d'inclure le menu et le template
-     */
-    $content = ob_get_clean(); 
-    require "../../partials/layout.php";
+/**
+ * Ne pa modifier
+ * permet d'inclure le menu et le template
+ */
+$content = ob_get_clean();
+require "../../partials/layout.php";
 ?>
