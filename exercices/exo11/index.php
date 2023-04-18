c5bf7d8d81628b6ea4068f08d7cb647ba9be3c03
Sylvain Torrenti <sylvain.torrenti@gmail.com>
2023-04-18 14:43:21 +0200

    exo 11

--- exercices/exo11/index.php
+++ exercices/exo11/index.php
@@ -1,17 +1,26 @@
 <?php ob_start(); //  ne pas modifier
-$titre = "Exo 11 - Boucles"; // Mettre le titre de la page
+$titre = "Exo 11 "; // Mettre le titre de la page
 ?>
 
 <!-- mettre votre code ici -->
 <?php
-
+$nbChercher = 245;
+$nbRand = rand(100, 999);
+$coup = 0;
+while (!($nbChercher == $nbRand)) {
+    echo "Tirage $coup : $nbRand <br>";
+    $coup++;
+    $nbRand = rand(100, 999);
+}
+echo "Tirage $coup : $nbRand <br>";
+echo "$nbChercher trouvé en $coup coups";
 ?>
 
 <?php
-    /**
-     * Ne pa modifier
-     * permet d'inclure le menu et le template
-     */
-    $content = ob_get_clean(); 
-    require "../../partials/layout.php";
+/**
+ * Ne pa modifier
+ * permet d'inclure le menu et le template
+ */
+$content = ob_get_clean();
+require "../../partials/layout.php";
 ?>
