3499693472bddb247cb32d35a0286283fa03f635
Sylvain Torrenti <sylvain.torrenti@gmail.com>
2023-04-18 14:36:12 +0200

    exo 10

--- exercices/exo10/index.php
+++ exercices/exo10/index.php
@@ -1,18 +1,26 @@
 <?php ob_start(); //  ne pas modifier
-$titre = "Exo 10- Boucles"; // Mettre le titre de la page
+$titre = "Exo 10"; // Mettre le titre de la page
 ?>
 
 <!-- mettre votre code ici -->
 <?php
-
+$i = 0;
+do {
+    $rand1 = rand(1, 1000);
+    $rand2 = rand(1, 1000);
+    $rand3 = rand(1, 1000);
+    echo "$rand1, $rand2, $rand3 <br>";
+    $i++;
+} while (!(($rand1 % 2 == 0) && ($rand2 % 2 != 0) && ($rand3 % 2 != 0)));
+echo "Resultat obtenu en $i coups";
 
 ?>
 
 <?php
-    /**
-     * Ne pa modifier
-     * permet d'inclure le menu et le template
-     */
-    $content = ob_get_clean(); 
-    require "../../partials/layout.php";
+/**
+ * Ne pa modifier
+ * permet d'inclure le menu et le template
+ */
+$content = ob_get_clean();
+require "../../partials/layout.php";
 ?>
