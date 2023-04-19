11c30bfb862fec0ea7c00b5dd5099a2ae74514ea
Sylvain Torrenti <sylvain.torrenti@gmail.com>
2023-04-19 15:15:26 +0200

    exo 19

--- exercices/exo19/index.php
+++ exercices/exo19/index.php
@@ -1,18 +1,30 @@
 <?php ob_start(); //  ne pas modifier
-$titre = "Exo 19 - Tableaux"; // Mettre le titre de la page
+$titre = "Exo 19"; // Mettre le titre de la page
 ?>
 
 <!-- mettre votre code ici -->
 <?php
+$personnes = ["p1" => ["nom" => "Clément", "age" => 25, "sexe" => true], "p2" => ["nom" => "Mathilde", "age" => 19, "sexe" => false]];
+foreach ($personnes as $key => $personne) {
 
+    foreach ($personne as $clef => $valeur) {
+        if ($personne["sexe"] == true) {
+            echo $clef . " : " . $valeur . "<br>";
+        } else {
+            echo $clef . " : " . $valeur . "<br>";
+        }
 
+    }
+    echo "=============";
+    echo "<br>";
+}
 ?>
 
 <?php
-    /**
-     * Ne pa modifier
-     * permet d'inclure le menu et le template
-     */
-    $content = ob_get_clean(); 
-    require "../../partials/layout.php";
+/**
+ * Ne pa modifier
+ * permet d'inclure le menu et le template
+ */
+$content = ob_get_clean();
+require "../../partials/layout.php";
 ?>
