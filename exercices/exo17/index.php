19e9ce4ef3e2154313fbdaa23d08577723c9acd4
Sylvain Torrenti <sylvain.torrenti@gmail.com>
2023-04-19 11:28:42 +0200

    exo 17

--- exercices/exo17/index.php
+++ exercices/exo17/index.php
@@ -1,16 +1,25 @@
 <?php ob_start(); //  ne pas modifier
-$titre = "Exo 17 - Tableaux"; // Mettre le titre de la page
+$titre = "Exo 17"; // Mettre le titre de la page
 ?>
 
 <!-- mettre votre code ici -->
 <?php
-?>
+$NoteMarc = [5, 14, 18, 2, 14, 12];
+$NoteLuc = [14, 15, 12, 14, 18, 19];
 
+$sum = array_sum($NoteMarc);
+$average = round($sum / count($NoteMarc));
+echo "La moyenne des notes de Marcs est de $average";
+echo "<br>";
+$sum = array_sum($NoteLuc);
+$average = $sum / count($NoteLuc);
+echo "La moyenne des notes de Luc est de $average";
+?>
 <?php
-    /**
-     * Ne pa modifier
-     * permet d'inclure le menu et le template
-     */
-    $content = ob_get_clean(); 
-    require "../../partials/layout.php";
+/**
+ * Ne pa modifier
+ * permet d'inclure le menu et le template
+ */
+$content = ob_get_clean();
+require "../../partials/layout.php";
 ?>
