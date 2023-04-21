ea129b7d87c7bf6fab4d7f6524ec10391f89d318
Sylvain Torrenti <sylvain.torrenti@gmail.com>
2023-04-21 11:15:55 +0200

    exo 24

--- exercices/exo24/index.php
+++ exercices/exo24/index.php
@@ -1,14 +1,25 @@
 <?php ob_start(); //  ne pas modifier
 $titre = "Exo 24 - Bonus"; // Mettre le titre de la page
 ?>
-
 <!-- mettre votre code ici -->
 <?php
-
+function multiplication(int $nb)
+{
+    $i = 1;
+    while ($i <= 10) {
+        $produit = $nb * $i;
+        echo "<td>$produit</td>";
+        $i = $i + 1;
+    }
+}
+echo "<table>";
+$a = 1;
+while ($a <= 10) {
+    echo "<tr>" . multiplication($a) . "</tr>";
+    $a++;
+}
+echo "</table>";
 ?>
-
-
-
 <?php
 /**
  * Ne pa modifier
