ff4f8c74afdd15eeaaf5a7b2da6989c5f79d291f
Sylvain Torrenti <sylvain.torrenti@gmail.com>
2023-04-21 09:58:40 +0200

    exo 23

--- exercices/exo23/index.php
+++ exercices/exo23/index.php
@@ -11,7 +11,22 @@ $titre = "Exo 23 - Formulaires"; // Mettre le titre de la page
 
 <!-- mettre votre code ici -->
 <?php
-
+$note1 = '';
+if (isset($_POST['nb1'])) {
+    $note1 = htmlentities($_POST['nb1']);
+}
+$note2 = '';
+if (isset($_POST['nb2'])) {
+    $note2 = htmlentities($_POST['nb2']);
+}
+$note3 = '';
+if (isset($_POST['nb3'])) {
+    $note3 = htmlentities($_POST['nb3']);
+}
+$notes = [$note1, $note2, $note3];
+$sum = array_sum($notes);
+$average = round($sum / count($notes));
+echo "La moyenne des notes des 3 notes est de $average";
 ?>
 
 <?php
