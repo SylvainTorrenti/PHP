5ab54bcddc0772caad60009c6631e58ff8f29202
Sylvain Torrenti <sylvain.torrenti@gmail.com>
2023-04-22 09:26:13 +0200

    exo 26

--- exercices/exo26/index.php
+++ exercices/exo26/index.php
@@ -1,10 +1,17 @@
 <?php ob_start(); //  ne pas modifier
-$titre = "Exo 26 - Bonus"; // Mettre le titre de la page
+$titre = "Exo 26"; // Mettre le titre de la page
 ?>
 
 <!-- mettre votre code ici -->
 <?php
-
+$Rantanplan = ["nom" => "Rantanplan", "âge" => 10, "type" => "chien"];
+$Rufus = ["nom" => "Rufus", "âge" => 15, "type" => "chien"];
+$Medor = ["nom" => "Medor", "âge" => 5, "type" => "chien"];
+$Diablo = ["nom" => "Diablo", "âge" => 8, "type" => "chat"];
+$Gribouille = ["nom" => "Gribouille", "âge" => 12, "type" => "chat"];
+$Sweety = ["nom" => "Sweety", "âge" => 19, "type" => "chat"];
+$animaux = [$Rantanplan, $Rufus, $Medor, $Diablo, $Gribouille, $Sweety];
+var_dump($animaux);
 ?>
 
 <?php
