e5b9a4b6b6d9b1df5cdd285239c05070496281e4
SylvainTorrenti <torrsyl@gmail.com>
2023-04-29 09:15:24 +0200

    commentaire

--- exercices/exo07/index.php
+++ exercices/exo07/index.php
@@ -4,6 +4,7 @@ $titre = "Exo 07 - Boucles"; // Mettre le titre de la page
 
 <!-- mettre votre code ici -->
 <?php
+// creation d'une variable random
 $rand = rand(5, 15);
 for ($i = 1; $i <= 10; $i++) {
     $res = $rand * $i;
