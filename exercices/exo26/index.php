2a6ef2fe3234817af9d3d39ed33f33b5662a4048
Sylvain Torrenti <sylvain.torrenti@gmail.com>
2023-05-03 15:47:38 +0200

    Exo 26

--- exercices/exo26/index.php
+++ exercices/exo26/index.php
@@ -3,16 +3,45 @@ $titre = "Exo 26"; // Mettre le titre de la page
 ?>
 
 <!-- mettre votre code ici -->
-
-<button name="TOUS" type="button"><a href="tous.php">TOUS</a></button>
-<button name="CHIENS" type="button"><a href="chiens.php">CHIENS</a></button>
-<button name="CHATS" type="button"><a href="chats.php">CHATS</a></button>
-<button name="RESET" type="reset"><a href="index.php">RESET</a></button>
 <?php
+$Rantanplan = ["nom" => "Rantanplan", "âge" => 10, "type" => "chien"];
+$Rufus = ["nom" => "Rufus", "âge" => 15, "type" => "chien"];
+$Medor = ["nom" => "Medor", "âge" => 5, "type" => "chien"];
+$Diablo = ["nom" => "Diablo", "âge" => 8, "type" => "chat"];
+$Gribouille = ["nom" => "Gribouille", "âge" => 12, "type" => "chat"];
+$Sweety = ["nom" => "Sweety", "âge" => 19, "type" => "chat"];
+$animaux = [$Rantanplan, $Rufus, $Medor, $Diablo, $Gribouille, $Sweety];
+?>
+<form action="" method="get">
+
+    <button name="choix" value="tous" type="submit">TOUS</button>
+    <button name="choix" value="chien" type="submit">CHIENS</button>
+    <button name="choix" value="chat" type="submit">CHATS</button>
+    <button name="choix" value="reset" type="submit">RESET</button>
+</form>
+
+<?php
+$choix = "tous";
+if (isset($_GET['choix'])) {
+    $choix = $_GET['choix'];
+}
+echo "<br>";
+foreach ($animaux as $key => $animal) {
+    if ($choix == "tous" || $choix == $animal['type']) {
+        foreach ($animal as $clef => $valeur) {
+            echo $clef . ' : ' . $valeur . '<br>';
+        }
+
+        echo "=============";
+        echo "<br>";
+    }
+}
 /**
  * Ne pa modifier
  * permet d'inclure le menu et le template
  */
 $content = ob_get_clean();
 require "../../partials/layout.php";
+
+
 ?>
