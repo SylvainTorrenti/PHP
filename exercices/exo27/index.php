62d1fdca02583108b95bb1338d1a8cfc904e8484
Sylvain Torrenti <sylvain.torrenti@gmail.com>
2023-05-04 09:17:53 +0200

    exo 27 correction

--- exercices/exo27/index.php
+++ exercices/exo27/index.php
@@ -11,7 +11,6 @@ $Medor = new Animal('Medor', 5, 'chien');
 $Diablo = new Animal('Diablo', 8, 'chat');
 $Gribouille = new Animal('Gribouille', 12, 'chat');
 $Sweety = new Animal('sweety', 19, 'chat');
-$animaux = [$Rantanplan, $Rufus, $Medor, $Diablo, $Gribouille, $Sweety];
 ?>
 <form action="" method="get">
 
