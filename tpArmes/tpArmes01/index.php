ebc380c0d562967f4a9a7aa7604ba6a13cdd18a2
Sylvain Torrenti <sylvain.torrenti@gmail.com>
2023-05-04 14:53:23 +0200

    tp arme 01

--- tpArmes/tpArmes01/index.php
+++ tpArmes/tpArmes01/index.php
@@ -4,9 +4,36 @@ $titre = "TP 01 - Les variables"; // Mettre le titre de la page
 
 <!-- mettre votre code ici -->
 <?php
-
+$arme1 = "arme 1 : épée";
+$arme2 = "arme 2 : arc";
+$arme3 = "arme 3 : hache";
+$arme4 = "arme 4 : fléau";
+?>
+<p><b>Voici toutes les armes</b></p>
+<?php
+echo $arme1 . "<br>";
+echo $arme2 . "<br>";
+echo $arme3 . "<br>";
+echo $arme4 . "<br>";
 ?>
 
+<form action="">
+    <select name="weapon" id="weapon">
+        <option value="">Choisir une arme</option>
+        <option value="epee">
+            épée
+        </option>
+        <option value="arc">
+            arc
+        </option>
+        <option value="hache">
+            hache
+        </option>
+        <option value="fleau">
+            fléau
+        </option>
+    </select>
+</form>
 
 
 <?php
