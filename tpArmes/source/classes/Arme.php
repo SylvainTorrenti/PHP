ae970a0fef11f9079bf2b90bc951bf5daf5e39c8
Sylvain Torrenti <sylvain.torrenti@gmail.com>
2023-05-12 11:41:28 +0200

    improve

--- tpArmes/source/classes/Arme.php
+++ tpArmes/source/classes/Arme.php
@@ -1,19 +1,23 @@
 <?php
 class Arme
 {
+    //Declaration des propriétés
     public $img;
     public $name;
     public $detail;
-
+    // creation d'un constructeur
     public function __construct($img, $name, $detail)
     {
         $this->img = $img;
         $this->name = $name;
         $this->detail = $detail;
     }
-
-
-
+    public function __toString()
+    {
+        $str = '<div class ="weaponProp"><div><img src="' . $this->img . '"></div>';
+        $str .= '<div class="description"><span class="weapon">' . $this->name . '</span><br>';
+        $str .= '<p>' . $this->detail . '</p><br></div></div><br>';
+        return $str;
+    }
 }
-
 ?>
--- tpArmes/tpArmes03/index.php
+++ tpArmes/tpArmes03/index.php
@@ -13,63 +13,19 @@ $titre = "TP 03 - La POO"; // Mettre le titre de la page
 
 <body>
     <?php
+    // appel du fichier contenant la classe Arme
     require '../../tpArmes/source/classes/Arme.php';
 
-
+    //creations des objet Arme
     $arme1 = new Arme("/tpArmes/source/epee/epee1.png", "épée", "Une arme tranchante");
     $arme2 = new Arme("/tpArmes/source/arc/arc1.png", "arc", "Une arme à distance");
     $arme3 = new Arme("/tpArmes/source/hache/hache1.png", "hache", "Une arme tranchante ou outil pour couper du bois");
     $arme4 = new Arme("/tpArmes/source/fleau/fleau1.png", "fleau", "Une arme contondante");
     echo "<b>Voici toutes les armes :</b><br>";
-    echo "<div class =\"weaponProp\">";
-    // foreach ($armes as $key => $arme) {
-    foreach ($arme1 as $clef => $valeur) {
-        if ($clef == 'img') {
-            echo "<div><img src=" . $valeur . "></div>";
-        } elseif ($clef == "name") {
-            echo "<div class=\"description\"><span class=\"weapon\">" . $valeur . '</span><br>';
-        } else {
-            echo "<p>" . $valeur . '</p><br></div>';
-        }
-    }
-    echo "</div>";
-    echo "<br>";
-    echo "<div class =\"weaponProp\">";
-    foreach ($arme2 as $clef => $valeur) {
-        if ($clef == 'img') {
-            echo "<div><img src=" . $valeur . "></div>";
-        } elseif ($clef == "name") {
-            echo "<div class=\"description\"><span class=\"weapon\">" . $valeur . '</span><br>';
-        } else {
-            echo "<p>" . $valeur . '</p><br></div>';
-        }
-    }
-    echo "</div>";
-    echo "<br>";
-    echo "<div class =\"weaponProp\">";
-    foreach ($arme3 as $clef => $valeur) {
-        if ($clef == 'img') {
-            echo "<div><img src=" . $valeur . "></div>";
-        } elseif ($clef == "name") {
-            echo "<div class=\"description\"><span class=\"weapon\">" . $valeur . '</span><br>';
-        } else {
-            echo "<p>" . $valeur . '</p><br></div>';
-        }
-    }
-    echo "</div>";
-    echo "<br>";
-    echo "<div class =\"weaponProp\">";
-    foreach ($arme4 as $clef => $valeur) {
-        if ($clef == 'img') {
-            echo "<div><img src=" . $valeur . "></div>";
-        } elseif ($clef == "name") {
-            echo "<div class=\"description\"><span class=\"weapon\">" . $valeur . '</span><br>';
-        } else {
-            echo "<p>" . $valeur . '</p><br></div>';
-        }
-    }
-    echo "</div>";
-
+    echo $arme1;
+    echo $arme2;
+    echo $arme3;
+    echo $arme4;
     ?>
 </body>
 
